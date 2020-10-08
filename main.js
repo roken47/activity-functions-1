d9809aeb08d1b66cc2edf863db0cffeb6cc965f4
roken47 <69013261+roken47@users.noreply.github.com>
2020-10-08 13:44:28 -0500

    init 7 easy functions and basic buttons for all functions in activity

---
+++ main.js
@@ -0,0 +1,8 @@
+// Easy Mode Function Buttons
+const sumBtn = document.querySelector("#sum");
+const avgBtn = document.querySelector("#avg");
+const greatThanBtn = document.querySelector("#greaterThan");
+const contVowelBtn = document.querySelector("#containsVowel");
+const tipBtn = document.querySelector("#tip");
+const longWordBtn = document.querySelector("#longestWord");
+// Functions for 7 Easy Exercises
